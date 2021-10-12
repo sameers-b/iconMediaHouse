545aebdc8bf65b2928ed711597fab4bc54f2cfe9
Sameer Singh <sameersingh7458@gmail.com>
2021-10-12 12:47:57 -0700

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+const Footer = () => {
+   return (
+      <>
+         <div className="footer-top">
+             <div className="footer-top-left">
+                <div>
+                   <span><h1>Logo</h1></span>
+                   <span>We are a team of creatives who are thrilled to create, ignite and succeed your business with all things digital</span>
+                </div>
+                <label>Email Us</label>
+                <p>sameersingh7458@gmail.com</p>
+                <label>Contact</label>
+                <p>+91 7458855350</p>
+             </div>
+             <div className="footer-top-right">
+                <label>Quick Links</label>
+                <ul>
+                   <li>About</li>
+                   <li>Careers</li>
+                   <li>Make Payment</li>
+                   <li>Reach Us</li>
+                </ul>
+             </div>
+         </div>
+         <div className="footer-bottom">
+
+         </div>
+      </>
+   )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+
+const Header = () => {
+   return (
+      <>
+         <div className="header-container">
+           <div className="header-logo">
+             <h1>Logo</h1>
+           </div>
+           <div className="header-right">
+              <ul>
+                 <li>About</li>
+                 <li>Gallary</li>
+                 <li><button>Get in touch</button></li>
+              </ul>
+           </div>
+         </div>
+      </>
+   )
+}
+
+export default Header
